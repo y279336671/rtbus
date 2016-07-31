f9bc03861f7b7d2f2f015884b42a01691dcdec93
XueBing <xuebing1110@gmail.com>
2016-07-31 19:24:47 +0800

    在路上...

--- mweb/js/line.js
+++ mweb/js/line.js
@@ -62,7 +62,7 @@ function renderLineInfo(){
                         $("#"+lastid).addClass("cd-bus");
                         $("#"+lastid).find("img").attr("src","vendor/images/bus2.png");
 
-                        $("#"+lastid).find("h2").html("...");
+                        $("#"+lastid).find("h2").html("在路上...");
                     }
 
                     //未到站 站点
