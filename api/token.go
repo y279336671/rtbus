1e6e6a365fc7650d8f76ca27ccb390c4ab563b3b
xuebing1 <xuebing1@le.com>
2016-08-04 18:41:49 +0800

    增加日志打印

--- api/token.go
+++ api/token.go
@@ -2,10 +2,13 @@ package api
 
 import (
 	//"fmt"
+	"github.com/bingbaba/util/logs"
 	"net/http"
 )
 
 func getToken() ([]*http.Cookie, error) {
+	logs.GetBlogger().Info("refresh token...")
+
 	resp, err := http.Get(URL_BJ_HOME)
 	if err != nil {
 		return make([]*http.Cookie, 0), err
