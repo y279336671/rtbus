5e9081e87178dd8176bc19b76e4ea4bd0c592b04
XueBing <xuebing1110@gmail.com>
2016-08-05 23:39:39 +0800

    调整字体大小

--- mweb/js/rtbus.js
+++ mweb/js/rtbus.js
@@ -68,6 +68,11 @@ function getbusline(){
 
                 showstation();
                 $('#loadingToast').hide();
+            }，
+            error:function(){
+                $("#busline").attr("placeholder",busline+"路公交暂不支持实时查询，请重新输入");
+                $("#rtbus_direction").focus();
+                $('#loadingToast').hide();
             }
         })
     }
